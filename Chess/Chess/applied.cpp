c0113f92f4f10139cf6e6e79ee8d0e465b291068
kjh99660 <kdh4021200@naver.com>
2019-06-07 17:29:48 +0900

    응용함수 최종본

--- Chess/Chess/applied.cpp
+++ Chess/Chess/applied.cpp
@@ -81,3 +81,111 @@ void drawLook(BlockID block, int x, int y, int z)
 	drawCube(air, x - 1, y + 21, z + 5, 3, 1, 2);
 	drawCube(air, x - 1, y + 21, z - 5, 3, 1, 2);
 }
+void drawKing(BlockID block, int midx, int y, int midz)
+{
+	int h = 0, r = 5;
+	drawCircle(block, midx, y + h, midz, r);
+	h++;
+	for (int i = 0; i < 3; i++, h++, r--)
+		drawCircle(block, midx, y + h, midz, r);
+	for (int i = 0, t = r; i < 4; i++, h++) {
+		r = t - i / 2;
+		drawCircle(block, midx, y + h, midz, r);
+	}
+
+	r--;
+	for (int i = 0; i < 3; i++, h++)
+		drawCircle(block, midx, y + h, midz, r);
+
+	r = 3;
+	for (int i = 0; i < 3; i++, h++, r--)
+		drawCircle(block, midx, y + h, midz, r);
+	for (int i = 0; i < 3; i++, h++, r++) {
+		drawCircle(block, midx, y + h, midz, r);
+	}
+	for (int i = 0; i < 3; i++, h++, r--)
+		drawCircle(block, midx, y + h, midz, r);
+
+	locateBlock(block, midx, y + h, midz);
+	h++;
+	for (int i = -1; i < 2; i++)
+		locateBlock(block, midx + i, y + h, midz);
+	h++;
+	locateBlock(block, midx, y + h, midz);
+}
+void drawPawn(BlockID block, int midx, int y, int midz)
+{
+	int h = 0, r = 4;
+	drawCircle(block, midx, y + h, midz, r);
+	h++;
+	for (int i = 0; i < 3; i++, h++, r--)
+		drawCircle(block, midx, y + h, midz, r);
+	for (int i = 0, t = r; i < 4; i++, h++) {
+		r = t - i / 2;
+		drawCircle(block, midx, y + h, midz, r);
+	}
+
+	r = 2;
+	drawCircle(block, midx, y + h, midz, r);
+
+	r = 3;
+	h = h + 3;
+	drawSphere(block, midx, y + h, midz, r);
+}
+void drawChessBoard(BlockID block, int x, int y, int z)
+{
+	drawCube(block, x, y, z, 10, 10, 3);
+	drawCube(block, x + 94, y, z, 10, 10, 3);
+	drawCube(block, x, y, z + 114, 10, 10, 3);
+	drawCube(block, x + 114, y, z + 114, 10, 10, 3);
+	drawCube(block, x, y + 3, z, 124, 124, 11);
+	drawPlaneLine(block, x, y + 14, z, 124, 124, 10);
+
+
+	WoolID white = createWool(COLOR_WHITE);
+	WoolID black = createWool(COLOR_BLACK);
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			drawPlane(white, x + 2 + 26 * i, y + 14, z + 2 + 26 * j, 13, 13);
+			drawPlane(black, x + 15 + 26 * i, y + 14, z + 2 + 26 * j, 13, 13);
+		}
+	}
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			drawPlane(black, x + 2 + 26 * i, y + 14, z + 15 + 26 * j, 13, 13);
+			drawPlane(white, x + 15 + 26 * i, y + 14, z + 15 + 26 * j, 13, 13);
+		}
+	}
+
+
+}
+void drawKnight(BlockID block, int x, int y, int z)
+{
+	for (int dy = 0; dy < 2; dy++)
+		drawCircle(block, x, y + dy, z, 6);
+	drawCircle(block, x, y + 2, z, 5);
+	drawCube(block, x - 3, y + 3, z - 3, 8, 7, 3);
+	drawCube(block, x - 1, y + 6, z - 3, 6, 7, 3);
+	drawCube(block, x + 1, y + 9, z - 3, 4, 7, 2);
+	drawCube(block, x - 3, y + 11, z - 3, 8, 7, 6);
+	drawCube(block, x - 6, y + 12, z - 2, 3, 5, 1);
+	drawCube(block, x - 6, y + 14, z - 2, 3, 5, 2);
+	drawCube(block, x - 1, y + 17, z - 3, 2, 1, 4);
+	drawCube(block, x - 1, y + 17, z + 3, 2, 1, 4);
+
+	for (int i = 0; i < 3; i++)
+	{
+		drawCube(block, x - 2 + 3 * i, y + 17, z - 1, 1, 3, 2);
+		drawCube(block, x - 3 + 3 * i, y + 19, z - 3, 1, 3, 1);
+	}
+
+	WoolID black = createWool(COLOR_BLACK);
+	drawCube(black, x - 1, y + 17, z - 4, 2, 1, 2);
+	drawCube(black, x - 1, y + 17, z + 4, 2, 1, 2);
+
+}
--- Chess/Chess/applied.h
+++ Chess/Chess/applied.h
@@ -6,3 +6,7 @@ void chess_bottom(BlockID block, int x, int y, int z);
 void drawQueen(BlockID block, int x, int y, int z);
 void drawBishop(BlockID block, int x, int y, int z);
 void drawLook(BlockID block, int x, int y, int z);
+void drawKing(BlockID block, int midx, int y, int midz);
+void drawPawn(BlockID block, int midx, int y, int midz);
+void drawChessBoard(BlockID block, int x, int y, int z);
+void drawKnight(BlockID block, int x, int y, int z);
